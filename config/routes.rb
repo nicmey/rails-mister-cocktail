ba3df854c844d738f1c2784ab9e4432e99910f42
Nicolas Meyer <myr.nicolas@gmail.com>
2018-08-17 11:14:46 +0200

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "cocktails#index"
   resources :cocktails, :only => [:new, :create, :index, :edit, :show] do
     resources :doses, only: [ :new, :create]
   end
